27fff80e654362c9c483ca63362c00bdfed4738e
Tim <ts@plusw.de>
2013-05-28 15:38:17 +0200

    missing string added

--- examples/create.pl
+++ examples/create.pl
@@ -1,7 +1,7 @@
 use VM::JiffyBox;
 
 my $jiffy = VM::JiffyBox->new(token => 'MyToken');
-my $box = $jiffy->get_vm();
+my $box = $jiffy->get_vm('MyBoxID');
 
 print "< JIFFY >\n";
 print $jiffy->token . "\n";

7d93681ba5a31175ab837df4f0d17a2bc7ce081c
Boris Däppen <bod@perl-services.de>
2013-06-06 21:43:27 +0200

    POD is now saying what is there, and not what isn't

--- lib/VM/JiffyBox/Box.pm
+++ lib/VM/JiffyBox/Box.pm
@@ -69,40 +69,13 @@ __END__
 
 =head1 PLEASE NOTE
 
-This module ist still under heavy development and a B<TRIAL> version.
-We do not recommend to use or even test it.
+This module ist still under heavy development and a B<TRIAL> release.
+We do not recommend to use it.
 
 =head1 SYNOPSIS
 
- use VM::JiffyBox;
-
- my $jiffy = VM::JiffyBox->new($token);
-
- my $box_id = $jiffy->get_id_from_name($box_name);
- my $box    = $jiffy->get_vm($box_id);
-
- my $backup_id = $box->get_backup_id();
- my $new_box   = $jiffy->create_vm($backup_id);
- $new_box->start();
-
- my $new_box_details = $new_box->get_details();
-
- $new_box->stop();
- $new_box->delete();
-
- 1;
-
-=head1 METHODS
-
-=head2 get_backup_id()
-
-=head2 get_details()
-
-=head2 start()
-
-=head2 stop()
-
-=head2 delete()
+See the C<examples> directory for examples of working code.
+Synopsis will come when first stable release is here.
 
 =head1 SEE ALSO
 

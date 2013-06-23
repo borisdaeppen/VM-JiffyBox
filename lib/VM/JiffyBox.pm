8f0c768c378cf38cf869a37d10b51d6f3db51051
Boris Däppen <boris_daeppen@bluewin.ch>
2013-06-23 14:49:22 +0200

    small fix for caching and loads of documentation in POD

--- lib/VM/JiffyBox.pm
+++ lib/VM/JiffyBox.pm
@@ -56,7 +56,8 @@ sub get_id_from_name {
     
     my $details = $self->get_details;
 
-    $self->last( $details );
+    $self->last         ( $details );
+    $self->details_cache( $details );
     
     foreach my $box (values %{$details->{result}}) {
         return $box->{id} if ($box->{name} eq $box_name);
@@ -123,21 +124,126 @@ __END__
 
 =encoding utf8
 
-=head1 PLEASE NOTE
-
-This module ist still under heavy development and a B<TRIAL> release.
-We do not recommend to use it.
-
 =head1 SYNOPSIS
 
-See the C<examples> directory for examples of working code.
-Synopsis will come when first stable release is here.
+ #############################
+ # CREAT A CLONE FROM BACKUP #
+ #############################
+ 
+ # stuff we need to know our self
+ my $auth_token = $ARGV[0];
+ my $box_name   = $ARGV[1];
+ my $clone_name = $ARGV[2];
+ 
+ # prepare connection to VM-Server
+ my $jiffy = VM::JiffyBox->new(token => $auth_token); 
+ 
+ # translate VM-Name (String) to ID (Number)
+ my $master_box_id = $jiffy->get_id_from_name($box_name);
+ 
+ say "master_box_id: $master_box_id";
+ 
+ # prepare connection to the VM
+ my $master_box = $jiffy->get_vm($master_box_id);
+ 
+ # collect information about the VM
+ my $backup_id  = $master_box->get_backups()->{result}->{daily}->{id};
+ my $plan_id    = $master_box->get_details()->{result}->{plan}->{id};
+ 
+ say "backup_id: $backup_id";
+ say "plan_id: $plan_id";
+ 
+ # create a clone of the VM
+ my $clone_box  = $jiffy->create_vm( $clone_name, $plan_id, $backup_id );
+ 
+ # abort if create failed
+ unless ($clone_box) {
+     # FAIL
+     die $jiffy->last->{messages}->[0]->{message};
+ }
+ 
+ # wait for the clone to be ready
+ do {
+     say "waiting for clone to get READY";
+     sleep 15; 
+ } while (not $clone_box->get_details->{result}->{status} eq 'READY');
+ 
+ # start the clone
+ $clone_box->start();
+
+(See the C<examples> directory for more examples of working code.)
+
+=head1 ERROR HANDLING
+
+All methods will return C<0> on failure, so you can check for this with a simple C<if> on the return value.
+If you need the error message you can use the cache and look into the attribute C<last>.
+The form of the message is open.
+It can contain a simple string, or also a hash.
+This depends on the kind of error.
+So if you want to be sure, just use L<Data::Dumper> to print it.
+
+=head1 CACHING
+
+There are possibilities to take advantage of caching functionality.
+If you have once called get_details(), the results will be stored in the attribute C<details_cache>.
+
+=head1 METHODS
+
+=head2 get_details
+
+Returns hashref with information about the hypervisor and its virtual machines.
+Takes no arguments.
+
+Results are cached in C<details_cache>.
+
+=head2 get_id_from_name
+
+Returns the ID for a specific virtual machine.
+Takes the name for the virtual machine as first argument.
+
+(Also updates the C<details_cache>)
+
+=head2 get_vm
+
+Returns an object-ref to an existing virtual machine (L<VM::JiffyBox::Box>).
+Takes the ID for the virtual machine as first argument.
+
+=head2 create_vm
+
+Creates a new virtual machine and returns an object-ref to it (L<VM::JiffyBox::Box>).
+Takes 3 arguments:
+
+=over
+
+=item 1
+
+name: The name for the new VM.
+
+=item 2
+
+plan_id: The ID for pricing.
+
+=item 3
+
+backup_id: Name of the backup-image to take.
+
+=back
+
+B<Note:> This methods interface could change in future releases, since it does not yet cover full functionality.
 
 =head1 SEE ALSO
 
 =over
 
-=item * L<https://github.com/tim-schwarz/VM-JiffyBox>
+=item *
+
+Source, contributions, patches: L<https://github.com/tim-schwarz/VM-JiffyBox>
+
+=item *
+
+This module is B<not> officially supported by or related to the company I<domainfactory> in Germany.
+However it aims to provide an interface to the API of their product I<JiffyBox>.
+So to use this module with success you should also B<read their API-Documentation>, available for registered users of their service.
 
 =back
 
--- lib/VM/JiffyBox/Box.pm
+++ lib/VM/JiffyBox/Box.pm
@@ -159,21 +159,119 @@ __END__
 
 =encoding utf8
 
-=head1 PLEASE NOTE
-
-This module ist still under heavy development and a B<TRIAL> release.
-We do not recommend to use it.
-
 =head1 SYNOPSIS
 
-See the C<examples> directory for examples of working code.
-Synopsis will come when first stable release is here.
+This module should be used together with L<VM::JiffyBox>.
+L<VM::JiffyBox> is the factory for producing objects of this module.
+However if you want to do it yourself:
+
+ my $box = VM::JiffyBox::Box->new(id => $box_id);
+
+ # set the hypervisor of the VM (e.g. ref to JiffyBox)
+ $box->hypervisor($ref);
+
+You then can do a lot of stuff with this box:
+
+ # get some info
+ my $backup_id  = $box->get_backups()->{result}->{daily}->{id};
+ my $plan_id    = $box->get_details()->{result}->{plan}->{id};
+
+ # get more info using the caching technique
+ my $state      = $box->last->{result}->{status}
+ # ... or
+ use Data::Dumper;
+ print Dumper( $box->backup_cache->{result} );
+
+ # start, stop, delete...
+ if ( $box->start ) {
+     print "VM started"
+ }
+
+ # and so on...
+
+(See also the SYNOPSIS of L<VM::JiffyBox> or the C<examples> directory for more examples of working code.)
+
+=head1 ERROR HANDLING
+
+All methods will return C<0> on failure, so you can check for this with a simple C<if> on the return value.
+If you need the error message you can use the cache and look into the attribute C<last>.
+The form of the message is open.
+It can contain a simple string, or also a hash.
+This depends on the kind of error.
+So if you want to be sure, just use L<Data::Dumper> to print it.
+
+=head1 CACHING
+
+There are possibilities to take advantage of caching functionality.
+The following caches are available:
+
+=over
+
+=item last
+
+Always contains the last information.
+
+=item backup_cache 
+
+Contains information of the last call to get_backups().
+
+=item details_cache
+
+Contains information of the last call to get_details().
+
+=item start_cache
+
+Contains information of the last call to start().
+
+=item stop_cache
+
+Contains information of the last call to stop().
+
+=item delete_cache
+
+Contains information of the last call to delete().
+
+=back
+
+=head1 METHODS
+
+=head2 get_details
+
+Returns hashref with information about the virtual machine.
+Takes no arguments.
+
+=head2 get_backups
+
+Returns hashref with information about the backups of the virtual machine.
+Takes no arguments.
+
+=head2 start
+
+Starts a virtual machine.
+It must be ensured (by you) that the machine has the state C<READY> before calling this.
+
+=head2 stop
+
+Stop a virtual machine.
+
+=head2 delete
+
+Delete a virtual machine.
+(Be sure that the VM has the appropriate state for doing so)
 
 =head1 SEE ALSO
 
 =over
 
-=item * L<https://github.com/tim-schwarz/VM-JiffyBox>
+=item *
+
+Source, contributions, patches: L<https://github.com/tim-schwarz/VM-JiffyBox>
+
+=item *
+
+This module is B<not> officially supported by or related to the company I<domainfactory> in Germany.
+However it aims to provide an interface to the API of their product I<JiffyBox>.
+So to use this module with success you should also B<read their API-Documentation>, available for registered users of their service.
 
 =back
 
